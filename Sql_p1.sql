7cf41c34414a4b234f2ac11f67927fe6b4d6fcb5
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-13 10:23:11 +0300

    sql_p.1

---
+++ Sql_p1.sql
@@ -0,0 +1,7 @@
+--SELECT title,description FROM film; --Q.1
+
+--SELECT * FROM film WHERE LENGTH >=60 AND LENGTH <=75; --Q.2
+--SELECT * FROM film WHERE rental_rate =0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99) --Q.3
+--SELECT last_name FROM customer WHERE first_name = 'Mary'; --Q.4
+
+--SELECT * FROM film WHERE LENGTH <=50 AND rental_rate !=2.99 AND rental_rate != 4.99; --Q.5
