ca81b2e8dfb2efa44a718c1a2e9907c1f61bc841
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-15 20:08:17 +0300

    Create sql_p5.sql

---
+++ sql_p5.sql
@@ -0,0 +1,5 @@
+--SELECT title,length FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5; Q.1
+--SELECT title,length FROM film WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5; --Q.2
+--SELECT last_name, store_id FROM customer WHERE store_id =1 ORDER BY last_name ASC LIMIT 4; --Q.3
+--ALP OZKAN
+
