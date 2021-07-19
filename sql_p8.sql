35ae0cbf3744d68fa24287330b3f922600769292
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-19 17:34:42 +0300

    Create sql_p8.sql

---
+++ sql_p8.sql
@@ -0,0 +1,101 @@
+-- CREATE TABLE test(
+--    id INTEGER,
+--    name VARCHAR(50),
+--    birthday DATE,
+--    email VARCHAR(100)
+-- );
+
+-- insert into test (id, name, birthday, email) values (1, 'Talyah Luetkemeyers', '27-Jan-2021', 'tluetkemeyers0@adobe.com');
+-- insert into test (id, name, birthday, email) values (2, 'Wally Grattage', '23-Jan-2021', 'wgrattage1@go.com');
+-- insert into test (id, name, birthday, email) values (3, 'Erastus Keen', '27-May-2021', 'ekeen2@surveymonkey.com');
+-- insert into test (id, name, birthday, email) values (4, 'Kriste Vizard', '13-Dec-2020', 'kvizard3@tripadvisor.com');
+-- insert into test (id, name, birthday, email) values (5, 'Astrid Halversen', '21-Mar-2021', 'ahalversen4@infoseek.co.jp');
+-- insert into test (id, name, birthday, email) values (6, 'Rozalie Yaakov', '05-Jan-2021', 'ryaakov5@de.vu');
+-- insert into test (id, name, birthday, email) values (7, 'Dara Sparrow', '24-Apr-2021', 'dsparrow6@reference.com');
+-- insert into test (id, name, birthday, email) values (8, 'Taffy Duggary', '28-Aug-2020', 'tduggary7@weather.com');
+-- insert into test (id, name, birthday, email) values (9, 'Asa Bread', '14-May-2021', 'abread8@nba.com');
+-- insert into test (id, name, birthday, email) values (10, 'Curr Nassi', '05-Aug-2020', 'cnassi9@marriott.com');
+-- insert into test (id, name, birthday, email) values (11, 'Lyn Fernyhough', '27-Mar-2021', 'lfernyhougha@vimeo.com');
+-- insert into test (id, name, birthday, email) values (12, 'Vivi Faulkes', '18-Oct-2020', 'vfaulkesb@macromedia.com');
+-- insert into test (id, name, birthday, email) values (13, 'Min Genge', '02-Aug-2020', 'mgengec@so-net.ne.jp');
+-- insert into test (id, name, birthday, email) values (14, 'Skelly Capon', '12-Dec-2020', 'scapond@indiegogo.com');
+-- insert into test (id, name, birthday, email) values (15, 'Birch Puddefoot', '25-Jul-2020', 'bpuddefoote@jiathis.com');
+-- insert into test (id, name, birthday, email) values (16, 'Tait Cisco', '13-Dec-2020', 'tciscof@buzzfeed.com');
+-- insert into test (id, name, birthday, email) values (17, 'Neron Vautre', '16-Nov-2020', 'nvautreg@goodreads.com');
+-- insert into test (id, name, birthday, email) values (18, 'Mireille Le Prevost', '16-Mar-2021', 'mleh@csmonitor.com');
+-- insert into test (id, name, birthday, email) values (19, 'Hana Barrell', '24-May-2021', 'hbarrelli@paypal.com');
+-- insert into test (id, name, birthday, email) values (20, 'Barclay Bolam', '08-Dec-2020', 'bbolamj@google.nl');
+-- insert into test (id, name, birthday, email) values (21, 'Gwenora Fincke', '14-Apr-2021', 'gfinckek@prweb.com');
+-- insert into test (id, name, birthday, email) values (22, 'Tobias Fernant', '01-May-2021', 'tfernantl@php.net');
+-- insert into test (id, name, birthday, email) values (23, 'Arri Berkowitz', '30-Aug-2020', 'aberkowitzm@sun.com');
+-- insert into test (id, name, birthday, email) values (24, 'Con Greetham', '01-Aug-2020', 'cgreethamn@cnbc.com');
+-- insert into test (id, name, birthday, email) values (25, 'Kinna Fleming', '06-Feb-2021', 'kflemingo@independent.co.uk');
+-- insert into test (id, name, birthday, email) values (26, 'Karmen Zanuciolii', '08-Jun-2021', 'kzanucioliip@unicef.org');
+-- insert into test (id, name, birthday, email) values (27, 'Marjy Parrott', '06-Oct-2020', 'mparrottq@indiatimes.com');
+-- insert into test (id, name, birthday, email) values (28, 'Mikaela Ryman', '11-Nov-2020', 'mrymanr@nytimes.com');
+-- insert into test (id, name, birthday, email) values (29, 'Efren Samweyes', '13-Mar-2021', 'esamweyess@mediafire.com');
+-- insert into test (id, name, birthday, email) values (30, 'Alfi Matten', '30-Jan-2021', 'amattent@t.co');
+-- insert into test (id, name, birthday, email) values (31, 'Kendrick Morey', '03-Apr-2021', 'kmoreyu@utexas.edu');
+-- insert into test (id, name, birthday, email) values (32, 'Nanci Sancto', '14-Nov-2020', 'nsanctov@lycos.com');
+-- insert into test (id, name, birthday, email) values (33, 'Christoper Richings', '14-Aug-2020', 'crichingsw@drupal.org');
+-- insert into test (id, name, birthday, email) values (34, 'Auguste Brassington', '17-Jun-2021', 'abrassingtonx@google.es');
+-- insert into test (id, name, birthday, email) values (35, 'Debera Paulack', '18-Jun-2021', 'dpaulacky@mapquest.com');
+-- insert into test (id, name, birthday, email) values (36, 'Eachelle Demanche', '06-Aug-2020', 'edemanchez@skype.com');
+-- insert into test (id, name, birthday, email) values (37, 'Chad Ferreiro', '09-Dec-2020', 'cferreiro10@ucla.edu');
+-- insert into test (id, name, birthday, email) values (38, 'Bart Philpots', '30-Oct-2020', 'bphilpots11@cbc.ca');
+-- insert into test (id, name, birthday, email) values (39, 'Payton Childrens', '07-Feb-2021', 'pchildrens12@lycos.com');
+-- insert into test (id, name, birthday, email) values (40, 'Oralia Ortmann', '11-Jun-2021', 'oortmann13@last.fm');
+-- insert into test (id, name, birthday, email) values (41, 'Ashely Melpuss', '10-Aug-2020', 'amelpuss14@intel.com');
+-- insert into test (id, name, birthday, email) values (42, 'Gerianna Knowlys', '06-Oct-2020', 'gknowlys15@washingtonpost.com');
+-- insert into test (id, name, birthday, email) values (43, 'Hilario Pollicatt', '19-Nov-2020', 'hpollicatt16@ft.com');
+-- insert into test (id, name, birthday, email) values (44, 'Tierney Scotti', '24-Feb-2021', 'tscotti17@nature.com');
+-- insert into test (id, name, birthday, email) values (45, 'Hali Cecchi', '27-Nov-2020', 'hcecchi18@sbwire.com');
+-- insert into test (id, name, birthday, email) values (46, 'Ree Abbie', '28-Dec-2020', 'rabbie19@globo.com');
+-- insert into test (id, name, birthday, email) values (47, 'Nicholas Mocquer', '29-Aug-2020', 'nmocquer1a@freewebs.com');
+-- insert into test (id, name, birthday, email) values (48, 'Elsi Mileham', '27-Oct-2020', 'emileham1b@tripadvisor.com');
+-- insert into test (id, name, birthday, email) values (49, 'Elva Latham', '20-Sep-2020', 'elatham1c@trellian.com');
+-- insert into test (id, name, birthday, email) values (50, 'Amara Wiggington', '01-Dec-2020', 'awiggington1d@miibeian.gov.cn');
+
+
+-- UPDATE test
+-- SET name ='alp oz',
+--     birthday='1900-01-01',
+-- 	email='alp@mail.com'
+--     WHERE id = 5;
+
+-- UPDATE test
+-- SET name ='ennie bae',
+--     birthday='1920-01-01',
+-- 	email='ennie@mail.com'
+--     WHERE id = 50;
+-- UPDATE test
+-- SET name ='jack nick',
+--     birthday='1980-07-01',
+-- 	email='j.nick@mail.com'
+--     WHERE id = 20;
+	
+-- UPDATE test
+-- SET name ='britney spear',
+--     birthday='1989-01-27',
+-- 	email='bspear@mail.com'
+--     WHERE id = 16;
+-- UPDATE test
+-- SET name ='chris paul',
+--     birthday='1990-09-011',
+-- 	email='cp3@mail.com'
+--     WHERE id = 3;
+
+-- DELETE FROM test
+-- WHERE id = 13;
+
+-- DELETE FROM test
+-- WHERE name = 'Asa Bread';
+
+-- DELETE FROM test
+-- WHERE birthday = '1900-01-01';
+
+-- DELETE FROM test
+-- WHERE email ='cp3@mail.com';
+
+	
+--ALP OZKAN
