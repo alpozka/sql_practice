49fb5f23fce0314795a1cfac9c00e3bdb190b07d
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-26 01:09:31 +0300

    Create sql_p9.sql

---
+++ sql_p9.sql
@@ -0,0 +1,7 @@
+--SELECT city, country FROM city INNER JOIN country ON city.country_id = country.country_id; --Q.1
+
+--SELECT payment_id, first_name, last_name FROM customer INNER JOIN payment ON payment.customer_id = customer.customer_id; --Q.2
+
+--SELECT rental_id, first_name, last_name  FROM rental INNER JOIN customer ON rental.customer_id = customer.customer_id; --Q.3
+
+--ALP OZKAN
