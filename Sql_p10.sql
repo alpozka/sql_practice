030a2de7b8fffbdb9b8735d1b7898f7dbcca3a8a
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-26 08:21:01 +0300

    Create Sql_p10.sql

---
+++ Sql_p10.sql
@@ -0,0 +1,4 @@
+--SELECT city, country FROM city LEFT JOIN country ON city.country_id = country.country_id; --Q.1
+--SELECT payment_id, first_name, last_name FROM customer RIGHT JOIN payment ON payment.customer_id = customer.customer_id;--Q.2
+--SELECT rental_id, first_name, last_name FROM customer FULL JOIN rental ON rental.customer_id = customer.customer_id;--Q.3
+--ALP OZKAN
