7ebb7e1086456ed6fa4188e7b181657d791b0475
alpozka <70730220+alpozka@users.noreply.github.com>
2021-07-26 17:54:58 +0300

    Create Sql_p11.sql

---
+++ Sql_p11.sql
@@ -0,0 +1,25 @@
+--Q.1
+
+-- SELECT first_name FROM actor
+
+-- UNION ALL
+
+-- SELECT first_name FROM customer;
+
+--Q.2
+
+--  SELECT first_name FROM actor
+ 
+--  UNION 
+
+--  SELECT first_name FROM customer; --Q.2
+
+
+
+
+
+--ALP OZKAN
+
+
+
+
